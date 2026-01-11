a08067b76910e51d2eb7fd2c7398abe1f7eea2ac
Arvin Cris <arvincarloramoscris@gmail.com>
2026-01-11 17:35:05 +0800

    building a very simple api

--- index.js
+++ index.js
@@ -34,10 +34,22 @@ const url = require('url');
 
 ////////////////////////////////////
 // SERVER
+
+const data = fs.readFileSync(`${__dirname}/starter/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
     const pathName = req.url;
     if (pathName === '/overview' || pathName === '/') res.end('This is the OVERVIEW');
     else if (pathName === '/product') res.end('This is the PRODUCT');
+    else if (pathName === '/api') {
+        res.writeHead(200, {
+            'Content-type': 'application/json'
+        })
+        res.end(data);
+    }
+
+
     else {
         res.writeHead(404, {
             'Content-type': 'text/html',
